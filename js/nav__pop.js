41bcaebff7725a394883d307839aed42b4218a0b
unknown <josuelpzti@gmail.com>
2023-03-25 10:16:38 -0400

    fix/form

--- js/nav__pop.js
+++ js/nav__pop.js
@@ -1,3 +1,4 @@
+// DOM<>
 let profile = document.querySelector(".profile");
 let profile_Title = profile.querySelector(".profile__title");
 let profile_Subtitle = profile.querySelector(".profile__subtitle");
@@ -7,10 +8,10 @@ let profile_FormTitle = profile.querySelector(".profile__form_title")
 let profile_FormSubtitle = profile.querySelector(".profile__form_subtitle");
 let profile_ClosedButon = profile.querySelector(".profile__form_button-closed")
 let profile_AddButton = profile.querySelector(".profile__form_button");
-
+// DOM</>
 
   // Abrir y Cerrar FORM <>
-profile_Button.addEventListener("click", function() {
+  profile_Button.addEventListener("click", function() {
   profile_FormContainer.classList.add("form_container_active");
 });
 
@@ -29,23 +30,24 @@ profile_Subtitle.insertAdjacentText("afterbegin", `${profile_FormSubtitle.value}
 
 
   // Edicion de Inpit y Form <>
-profile_FormTitle.addEventListener("input", handleProfileFormSubmit);
-profile_FormSubtitle.addEventListener("input", handleProfileFormSubmit);
+profile_FormTitle.addEventListener("input", checkEventInput);
+profile_FormSubtitle.addEventListener("input", checkEventInput);
 
-function  handleProfileFormSubmit (){
+function  checkEventInput (){
+  profile_Title.textContent = profile_FormTitle.value
+  profile_Subtitle.textContent = profile_FormSubtitle.value
+}
+  // Edicion de Inpit y Form </>
+
+// GUARDAR LA INFORMACION <>
+function handleProfileFormSubmit (evt){
+  evt.handleProfileFormSubmit()
   profile_Title.textContent = profile_FormTitle.value
   profile_Subtitle.textContent = profile_FormSubtitle.value
 }
 
-
-// GUARDAR LA INFORMACION
-function GUARDAR (){
-  profile_Title.textContent += profile_FormTitle.value
-  profile_Subtitle.textContent += profile_FormSubtitle.value
-}
-
-profile_AddButton.addEventListener("sumit", GUARDAR)
-
+profile_AddButton.addEventListener("click", handleProfileFormSubmit)
+// GUARDAR LA INFORMACION </>
 
 
 
