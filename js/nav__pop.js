c2b1b8f54962291354dbb2a4cba91b3734dbd13c
unknown <josuelpzti@gmail.com>
2023-03-27 08:35:36 -0400

    form/fix

--- js/nav__pop.js
+++ js/nav__pop.js
@@ -1,26 +1,38 @@
 // DOM<>
+let page = document.querySelector(".page")
 let profile = document.querySelector(".profile");
 let profile_Title = profile.querySelector(".profile__title");
 let profile_Subtitle = profile.querySelector(".profile__subtitle");
 let profile_Button = profile.querySelector(".profile__button")
-let profile_FormContainer = profile.querySelector(".profile__form_container");
-let profile_FormTitle = profile.querySelector(".profile__form_title")
-let profile_FormSubtitle = profile.querySelector(".profile__form_subtitle");
-let profile_ClosedButon = profile.querySelector(".profile__form_button-closed")
-let profile_AddButton = profile.querySelector(".profile__form_button");
-// DOM</>
+let profile_FormContainer = document.querySelector(".profile__form_container");
+let profile_FormTitle = document.querySelector(".profile__form_title")
+let profile_FormSubtitle = document.querySelector(".profile__form_subtitle");
+let profile_ClosedButon = document.querySelector(".profile__form_button-closed")
+let profile_AddButton = document.querySelector(".profile__form_button");
+let element = document.querySelector(".main__element_container")
+let like = element.querySelectorAll(".element__like");
 
-  // Abrir y Cerrar FORM <>
+
+// Efecto Button Like
+like.forEach(function(item) {
+  item.addEventListener("click", function() {
+    item.classList.toggle("element__like_black");
+  });
+});
+
+
+// Abrir y Cerrar FORM <>
   profile_Button.addEventListener("click", function() {
   profile_FormContainer.classList.add("form_container_active");
+  page.classList.add("page__opacity")
 });
 
 profile_ClosedButon.addEventListener("click", function(){
   profile_FormContainer.classList.remove("form_container_active")
+  page.classList.remove("page__opacity")
 })
-// Abrir y Cerrar FORM </>
 
-  // Valores de Input y FORM <>
+// Valores de Input y FORM <>
 profile_FormTitle.value = "Jacques Cousteau"
 profile_FormSubtitle.value = "Explorador"
 
@@ -28,68 +40,13 @@ profile_Title.insertAdjacentText("afterbegin", `${profile_FormTitle.value}`);
 profile_Subtitle.insertAdjacentText("afterbegin", `${profile_FormSubtitle.value}`);
 // Valores de Input y FORM <>
 
-
-  // Edicion de Inpit y Form <>
-profile_FormTitle.addEventListener("input", checkEventInput);
-profile_FormSubtitle.addEventListener("input", checkEventInput);
-
-function  checkEventInput (){
-  profile_Title.textContent = profile_FormTitle.value
-  profile_Subtitle.textContent = profile_FormSubtitle.value
-}
-  // Edicion de Inpit y Form </>
-
 // GUARDAR LA INFORMACION <>
 function handleProfileFormSubmit (evt){
-  evt.handleProfileFormSubmit()
+  evt.preventDefault()
   profile_Title.textContent = profile_FormTitle.value
   profile_Subtitle.textContent = profile_FormSubtitle.value
+  profile_FormContainer.classList.remove("form_container_active")
+  page.classList.remove("page__opacity")
 }
 
 profile_AddButton.addEventListener("click", handleProfileFormSubmit)
-// GUARDAR LA INFORMACION </>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (OTRA FORMA DE HACERLO"Son Notas para mi Señor Revisor =)") function profileFormOpen() {
-//   profile_FormContainer.classList.toggle("form_container_active");
-//   return
-// };
-
-// profileFormOpen();
-
-// profile_Button.addEventListener("click", profileFormOpen);
