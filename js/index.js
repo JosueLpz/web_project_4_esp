8430ea64004d6b519133ea0349f2e29f7832808b
unknown <josuelpzti@gmail.com>
2023-04-19 09:21:19 -0400

    fix/form

--- js/index.js
+++ js/index.js
@@ -19,22 +19,39 @@ const formCardAdd = document.querySelector(".form__container_name_card");
 const formImgAdd = document.querySelector(".form__container_link_img");
 const formElementButtom = document.querySelector(".form__container_element_button");
 const Formtest = document.querySelector(".form__element");
+const buttonClosedElement = document.querySelector(".form__container_closed_element");
 
+// Valor de los Inpunts del form  START
 
-// Valores Inputs Form profile
-formCardAdd.value = "Hey"
-formImgAdd.value = "Soy Yo"
-
-// Valor de los Inpunts del form
-function imfoValueForm (){
+// asignando el valor de los inputs y añadiendolos al  dom
 formInfoName.value = "Marco Aurelio"
 formInfoHobbie.value = "Filosofo Emperador Romano"
-profileName.textContent = formInfoName.value;
-profileHobbie.textContent = formInfoHobbie.value;
-}
-imfoValueForm ()
+profileName.textContent = formInfoName.value
+profileHobbie.textContent = formInfoHobbie.value
 
-// Array de objetos de cartas de element
+// Evento para cambiar los valores de el elemento html
+formAddButton.addEventListener("click", function(evt){
+evt.preventDefault()
+
+// valores por defecto de el dom
+profileName.textContent = formInfoName.value
+profileHobbie.textContent = formInfoHobbie.value
+
+// cambio de valores por los agregados por el usuario
+formInfoName.value = profileName.textContent;
+formInfoHobbie.value = profileHobbie.textContent;
+
+// remover cambio de efectos
+formUp.classList.remove("form__container_open_active_e")
+page.classList.remove("page__opacity_active")
+});
+
+// Valor de los Inpunts del form END
+
+
+// Array de objetos de cartas de element START
+
+// array de objetos con las cartas para el dom element
 const initialCards = [
   {
     name: "Acrópolis Atenas",
@@ -48,32 +65,29 @@ const initialCards = [
     name: "Forum Romanum",
     link: "https://i.postimg.cc/hP9cnGJ9/Forum-Romanum-Rome-Italy.jpg"
   },
-  {
-    name: "Atenas Grecia",
-    link: "https://i.postimg.cc/nhpZht7N/la-acropolis-athenas-grecia.jpg"
-  },
-  {
-    name: "Trevi Fountain",
-    link: "https://i.postimg.cc/43FTP7MT/Trevi-Fountain-Roma-Italy.jpg"
-    
-  },
-  {
-    name: "Areopagitou Athens",
-    link: "https://i.postimg.cc/HLLgb462/Odeon-of-Herodes-Atticus-Dionysiou-Areopagitou-Athens-Greece.jpg"
-  },  
+  
 ];
 // TODO El ultmio elemento de CARD no se ve REVISAR LUEGO
 
+// valores de los inputs de el form de element
+formCardAdd.value = "Nombre del lugar"
+formImgAdd.value = "Link de el lugar"
+
+// iteracion de los objetos de el array con el metodo MAP y añadiendo las card
 initialCards.map(function (item){
+  // creando las cartas
   const templateElement = document.querySelector("#template__article").content;
   const templateCard = templateElement.querySelector(".element__article").cloneNode(true);
   const element = document.querySelector(".element")
   
+  // asignando los valores de el array de objetos a los elementos en cada iteracion
   templateElement.querySelector(".element__article_img").src = item.link
   templateElement.querySelector(".element__article_row_title").textContent = item.name
   
+  // asignando la posicion de las cartas en el elemento DOM
   element.prepend(templateCard);
   
+  // retorna las cartas con los elementos
   return templateCard
 });
 
@@ -97,14 +111,11 @@ formElementButtom.addEventListener("click", function(evt){
 });
 
 
-
-
-
 // Open Closed FORM
 function openPop (buttom, form){
   buttom.addEventListener("click", function(e) {
     console.log(e.target)
-    form.classList.add("form__container_open_active");
+    form.classList.add("form__container_open_active_e");
     page.classList.add("page__opacity_active")
   });
 }
@@ -112,7 +123,7 @@ openPop(profileButtonEdit, formUp);
 
 function closedPop(buttom, form){
   buttom.addEventListener("click", function(){
-  form.classList.remove("form__container_open_active")
+  form.classList.remove("form__container_open_active_e")
   page.classList.remove("page__opacity_active")
   });
 }
@@ -120,10 +131,9 @@ closedPop(formClosedButon, formUp);
 
 // Open Closed FORM Element
 openPop(profileButtonCardAdd, formElement);
+closedPop(buttonClosedElement, formElement);
 
-
-
-
+// todo Porque el form no funciono con el active de form. pero si con el de element?
 
 
 
@@ -148,10 +158,3 @@ openPop(profileButtonCardAdd, formElement);
 
 
 // // GUARDAR LA INFORMACION <>
-// function handleProfileFormSubmit (evt){
-//   evt.preventDefault()
-//   profileName.textContent = formInfoName.value
-//   profileHobbie.textContent = formInfoHobbie.value
-//   formUp.classList.remove("form__container_open_active")
-//   page.classList.remove("page__opacity_active")
-// }
