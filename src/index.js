ba6da9988c786901a3bb47f766ec972c90a30459
unknown <josuelpzti@gmail.com>
2023-06-09 09:21:43 -0400

    start/webpack

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/index.css";
-import stepsSrc from "./images/steps.png";
-
-const numbers = [2, 3, 5];
-const doubledNumbers = numbers.map((number) => number * 2);
-
-const stepsImage = document.getElementById("image-steps");
-stepsImage.src = stepsSrc;
