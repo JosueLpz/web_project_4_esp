57ef0d339462e3b7e1995f18b085e6dfdfceaa6a
unknown <josuelpzti@gmail.com>
2023-06-12 07:26:58 -0400

    bugfixhtmlfinish

--- src/js/PopupWithImage.js
+++ src/js/PopupWithImage.js
@@ -4,5 +4,6 @@ export default class PopupWithImage extends Popup {
   data(img, text) {
     document.querySelector(".zoom__img").src = img;
     document.querySelector(".zoom__text").textContent = text;
+    document.querySelector(".zoom__img").alt = text;
   }
 }
