020f05d180b3d3750668554364b18ad9776d8aa6
unknown <josuelpzti@gmail.com>
2023-06-30 18:58:30 -0400

    likes/70%

--- src/js/Card.js
+++ src/js/Card.js
@@ -49,6 +49,7 @@ export default class Card {
 
     return this._cardElement;
   }
+
   _likeCard() {
     const api = new Api({
       baseUrl: "users/me",
@@ -77,7 +78,20 @@ export default class Card {
               "Content-Type": "application/json",
             },
           });
-          return api.putLikesCard();
+          return api
+            .putLikesCard()
+            .then((res) => {
+              if (res.ok) {
+                return res.json();
+              }
+              return Promise.reject(`Error: ${res.status}`);
+            })
+            .then((res) => {
+              console.log("🚀 ~ file: Card.js:90 ~ Card ~ .then ~ res:", res);
+              this._likes = res.likes;
+              this._cardElement.querySelector(".element__article_row_like_counter").textContent = this._likes.length;
+              this._cardElement.querySelector(".element__article_row_like").classList.add("element__article_row_like_active");
+            });
         } else if (this._likes.some((like) => like._id === this._userID)) {
           const api = new Api({
             method: "DELETE",
@@ -87,98 +101,26 @@ export default class Card {
               "Content-Type": "application/json",
             },
           });
-          return api.deleteLikesCard(); // Utiliza la función correcta para eliminar el "me gusta"
-        }
-      })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Error: ${res.status}`);
-      })
-      .then((res) => {
-        this._likes = res.likes;
-        this._cardElement.querySelector(".element__article_row_like_counter").textContent = this._likes.length;
-        if (this._likes.some((like) => like._id === this._userID)) {
-          this._cardElement.querySelector(".element__article_row_like").classList.add("element__article_row_like_active");
-        } else {
-          this._cardElement.querySelector(".element__article_row_like").classList.remove("element__article_row_like_active");
+          return api
+            .deleteLikesCard()
+            .then((res) => {
+              if (res.ok) {
+                return res.json();
+              }
+              return Promise.reject(`Error: ${res.status}`);
+            })
+            .then((res) => {
+              console.log("🚀 ~ file: Card.js:113 ~ Card ~ .then ~ res:", res);
+              this._likes = res.likes;
+              this._cardElement.querySelector(".element__article_row_like_counter").textContent = this._likes.length;
+              this._cardElement.querySelector(".element__article_row_like").classList.remove("element__article_row_like_active");
+            });
         }
       })
       .catch((error) => {
         console.log("Error:", error);
       });
   }
-
-  // _likeCard() {
-  //   const api = new Api({
-  //     baseUrl: "users/me",
-  //     headers: {
-  //       authorization: "a1e6aa2e-20ff-4c9e-8a8e-2b23e3b6a743",
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  //   api
-  //     .getProfileUser()
-  //     .then((res) => {
-  //       if (res.ok) {
-  //         return res.json();
-  //       }
-  //       return Promise.reject(`Error: ${res.status}`);
-  //     })
-  //     .then((result) => {
-  //       this._userID = result._id;
-
-  //       if (this._likes.some((like) => like._id !== this._userID)) {
-  //         const api = new Api({
-  //           method: "PUT",
-  //           baseUrl: `cards/likes/${this._idCard}`,
-  //           headers: {
-  //             authorization: "a1e6aa2e-20ff-4c9e-8a8e-2b23e3b6a743",
-  //             "Content-Type": "application/json",
-  //           },
-  //         });
-  //         return api
-  //           .putLikesCard()
-  //           .then((res) => {
-  //             if (res.ok) {
-  //               return res.json();
-  //             }
-  //             return Promise.reject(`Error: ${res.status}`);
-  //           })
-  //           .then((res) => {
-  //             this._likes = res.likes;
-  //             this._cardElement.querySelector(".element__article_row_like_counter").textContent = this._likes.length;
-  //             this._cardElement.querySelector(".element__article_row_like").classList.add("element__article_row_like_active");
-  //           });
-  //       } else if (this._likes.some((like) => like._id === this._userID)) {
-  //         const api = new Api({
-  //           method: "DELETE",
-  //           baseUrl: `cards/likes/${this._idCard}`,
-  //           headers: {
-  //             authorization: "a1e6aa2e-20ff-4c9e-8a8e-2b23e3b6a743",
-  //             "Content-Type": "application/json",
-  //           },
-  //         });
-  //         return api
-  //           .deleteLikesCard()
-  //           .then((res) => {
-  //             if (res.ok) {
-  //               return res.json();
-  //             }
-  //             return Promise.reject(`Error: ${res.status}`);
-  //           })
-  //           .then((res) => {
-  //             this._likes = res.likes;
-  //             this._cardElement.querySelector(".element__article_row_like_counter").textContent = this._likes.length;
-  //             this._cardElement.querySelector(".element__article_row_like").classList.remove("element__article_row_like_active");
-  //           });
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error:", error);
-  //     });
-  // }
   _deleteCard() {
     this._cardElement.closest(".element__article").remove();
   }
