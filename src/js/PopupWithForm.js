927e9870c195f5f4969ee634dbdb1d37d1a9f587
unknown <josuelpzti@gmail.com>
2023-07-11 06:48:11 -0400

    info/show/userinfo

--- src/js/PopupWithForm.js
+++ src/js/PopupWithForm.js
@@ -1,5 +1,5 @@
 import Popup from "./Popup.js";
-import api from "./Api.js";
+import UserInfo from "./UserInfo.js";
 export default class PopupWithForm extends Popup {
   constructor({ elementSelector, handleFormSubmit, buttonSelector }) {
     super(elementSelector);
@@ -34,34 +34,10 @@ export default class PopupWithForm extends Popup {
     });
     return this._formValues;
   }
-  showInfoValue() {
-    api
-      .getProfileUser("users/me")
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Error: ${res.status}`);
-      })
-      .then((result) => {
-        const inputName = document.querySelector(".form__container-name");
-        const inputHobby = document.querySelector(".form__container-hobby");
-        const profileName = document.querySelector(".profile__row-name");
-        const profileHobby = document.querySelector(".profile__hobbie");
-        const avatar = document.querySelector(".profile__img");
-        avatar.src = result.avatar;
-        inputName.value = result.name;
-        inputHobby.value = result.about;
-        profileName.textContent = result.name;
-        profileHobby.textContent = result.about;
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-  }
   setEventListeners() {
+    const userInfo = new UserInfo({ title: "", hobby: "" });
+    userInfo.showInfoValue();
     super.setEventListeners();
-    this.showInfoValue();
     this._elementSelector.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
