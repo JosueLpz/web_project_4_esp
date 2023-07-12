2d79dd08429a7f4df3fcceae3dc68db7f761ac12
unknown <josuelpzti@gmail.com>
2023-07-12 10:12:21 -0400

    done/done

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,7 +3,7 @@ const zoomContainer = document.querySelector(".zoom");
 const formElement = document.querySelector(".card");
 const formUp = document.querySelector(".form");
 const formSwtichAvatar = document.querySelector(".avatar");
-const formDelete = document.querySelector(".popup__card");
+const formDelete = document.querySelector(".confirm");
 
 const popupElements = [formElement, formUp, zoomContainer, formSwtichAvatar, formDelete];
 
